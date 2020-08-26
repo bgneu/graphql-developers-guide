73d141fcacfde332e680a53e05685f64437da16b
Bruno Neu <brunoneu1110@gmail.com>
2020-08-26 11:28:08 -0400

    feat: Display a list of song titles
    Query GraphQL for song titles and display them in list format.

--- section-2/Lyrical-GraphQL/client/components/SongList.js
+++ section-2/Lyrical-GraphQL/client/components/SongList.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
 
-const SongList = () => {
+const SongList = (props) => {
+  const renderSongs = () => {
+    return props.data.songs.map(song => {
+      return (
+        <li>
+          {song.title}
+        </li>
+      )
+    })
+  }
+
   return (
     <div>
-      SongList
+      {props.data.loading ? 'Loading...' : renderSongs()}
     </div>
   )
 }
 
-export default SongList
+const query = gql`
+  {
+    songs {
+      title
+    }
+  }
+`
+
+export default graphql(query)(SongList)
