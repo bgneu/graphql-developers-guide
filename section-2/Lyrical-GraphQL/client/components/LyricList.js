c1fc8fd44d2bb00afa54603157fda9fb7491b91d
Bruno Neu <brunoneu1110@gmail.com>
2020-09-18 11:34:46 -0400

    feat: Create Lyric list

---
+++ section-2/Lyrical-GraphQL/client/components/LyricList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
+import { Link } from 'react-router'
+
+class LyricList extends Component {
+  render() {
+    return (
+      <ul>
+        LyricList
+      </ul>
+    )
+  }
+
+}
+
+export default LyricList
